6c6d3a873e13a18e099656d19ac70fb2b01b23b0
BC-macair013 <bc-macair013@BCmacair013sAir.zyxel.com>
2022-05-20 09:14:28 +0300

    Added language setting

--- src/components/Form.js
+++ src/components/Form.js
@@ -28,6 +28,7 @@ const validate = (values) => {
 const Form = () => {
 	const [isSubmitted, setIsSubmitted] = useState(false);
 	const [isDisabled, setIsDisabled] = useState(true);
+  const [language, setLanguage] = useState('FI');
 	const commentCharacterLimit = 500;
 
   const navigate = useNavigate();
@@ -89,203 +90,268 @@ const Form = () => {
     disabled: isSubmitted,
   };
 
-
+  const changeHandler = (e) => {
+    setLanguage(e.target.value);
+  }
+  console.log(language);
 	return (
 		<Fragment>
 			<CssBaseline />
 			<Container maxWidth="sm">
-				<Paper elevation={3} sx={{ minheight: '50vh' }}>
-					<Typography variant="h6" marginBottom={3}>
-						How likely are you to recommend PHZ Full Stack as an employer to a
-						friend or colleague?
-					</Typography>
+				<Paper elevation={3} sx={{ minheight: "50vh" }}>
+					<div>
+						<label htmlFor="response-sort" />
+						<select name="language" id="language" onChange={changeHandler}>
+							<option value="FI">FI</option>
+							<option value="EN">EN</option>
+							<option value="SV">SV</option>
+						</select>
+					</div>
+					{language === "FI" ? (
+						<Typography variant="h6" marginBottom={3}>
+							Kuinka todennäköisesti suosittelisit PHZ Full Stackia työnantajana
+							a ystävä vai kollega?
+						</Typography>
+					) : language === "EN" ? (
+						<Typography variant="h6" marginBottom={3}>
+							How likely are you to recommend PHZ Full Stack as an employer to a
+							friend or colleague?
+						</Typography>
+					) : language === "SV" ? (
+						<Typography variant="h6" marginBottom={3}>
+							Hur troligt är det att du rekommenderar PHZ Full Stack som
+							arbetsgivare till envän eller kollega?
+						</Typography>
+					) : (
+						""
+					)}
 
-          <form>
-            <Stack
-              sx={{
-                alignItems: "center",
-              }}
-              direction={{
-                xs: "column",
-                sm: "row",
-                md: "row",
-                lg: "row",
-                xl: "row",
-              }}
-            >
-              <ToggleButtonGroup
-                sx={{
-                  m: {
-                    xs: "12px",
-                    sm: "0px",
-                    md: "0px",
-                    lg: "0px",
-                    xl: "0px",
-                  },
-                }}
-                {...control}
-              >
-                <ToggleButton
-                  value={0}
-                  aria-label="0"
-                  name="score"
-                  sx={{
-                    width: "45px",
-                    height: "45px",
-                    borderRadius: 0,
-                    border: "1px darkgray solid",
-                  }}
-                >
-                  0
-                </ToggleButton>
-                <ToggleButton
-                  value={1}
-                  aria-label="1"
-                  name="score"
-                  sx={{
-                    width: "45px",
-                    height: "45px",
-                    border: "1px darkgray solid",
-                  }}
-                >
-                  1
-                </ToggleButton>
-                <ToggleButton
-                  value={2}
-                  aria-label="2"
-                  name="score"
-                  sx={{
-                    width: "45px",
-                    height: "45px",
-                    border: "1px darkgray solid",
-                  }}
-                >
-                  2
-                </ToggleButton>
-                <ToggleButton
-                  value={3}
-                  aria-label="3"
-                  name="score"
-                  sx={{
-                    width: "45px",
-                    height: "45px",
-                    border: "1px darkgray solid",
-                  }}
-                >
-                  3
-                </ToggleButton>
-                <ToggleButton
-                  value={4}
-                  aria-label="4"
-                  name="score"
-                  sx={{
-                    width: "45px",
-                    height: "45px",
-                    borderRadius: 0,
-                    border: "1px darkgray solid",
-                  }}
-                >
-                  4
-                </ToggleButton>
-              </ToggleButtonGroup>
-              <ToggleButtonGroup {...control}>
-                <ToggleButton
-                  value={5}
-                  aria-label="5"
-                  name="score"
-                  sx={{
-                    width: "45px",
-                    height: "45px",
-                    borderRadius: 0,
-                    ml: "-1px",
-                    border: "1px darkgray solid",
-                  }}
-                >
-                  5
-                </ToggleButton>
+					<form>
+						<Stack
+							sx={{
+								alignItems: "center",
+							}}
+							direction={{
+								xs: "column",
+								sm: "row",
+								md: "row",
+								lg: "row",
+								xl: "row",
+							}}
+						>
+							<ToggleButtonGroup
+								sx={{
+									m: {
+										xs: "12px",
+										sm: "0px",
+										md: "0px",
+										lg: "0px",
+										xl: "0px",
+									},
+								}}
+								{...control}
+							>
+								<ToggleButton
+									value={0}
+									aria-label="0"
+									name="score"
+									sx={{
+										width: "45px",
+										height: "45px",
+										borderRadius: 0,
+										border: "1px darkgray solid",
+									}}
+								>
+									0
+								</ToggleButton>
+								<ToggleButton
+									value={1}
+									aria-label="1"
+									name="score"
+									sx={{
+										width: "45px",
+										height: "45px",
+										border: "1px darkgray solid",
+									}}
+								>
+									1
+								</ToggleButton>
+								<ToggleButton
+									value={2}
+									aria-label="2"
+									name="score"
+									sx={{
+										width: "45px",
+										height: "45px",
+										border: "1px darkgray solid",
+									}}
+								>
+									2
+								</ToggleButton>
+								<ToggleButton
+									value={3}
+									aria-label="3"
+									name="score"
+									sx={{
+										width: "45px",
+										height: "45px",
+										border: "1px darkgray solid",
+									}}
+								>
+									3
+								</ToggleButton>
+								<ToggleButton
+									value={4}
+									aria-label="4"
+									name="score"
+									sx={{
+										width: "45px",
+										height: "45px",
+										borderRadius: 0,
+										border: "1px darkgray solid",
+									}}
+								>
+									4
+								</ToggleButton>
+							</ToggleButtonGroup>
+							<ToggleButtonGroup {...control}>
+								<ToggleButton
+									value={5}
+									aria-label="5"
+									name="score"
+									sx={{
+										width: "45px",
+										height: "45px",
+										borderRadius: 0,
+										ml: "-1px",
+										border: "1px darkgray solid",
+									}}
+								>
+									5
+								</ToggleButton>
 
-                <ToggleButton
-                  value={6}
-                  aria-label="6"
-                  name="score"
-                  sx={{
-                    width: "45px",
-                    height: "45px",
-                    border: "1px darkgray solid",
-                  }}
-                >
-                  6
-                </ToggleButton>
-                <ToggleButton
-                  value={7}
-                  aria-label="7"
-                  name="score"
-                  sx={{
-                    width: "45px",
-                    height: "45px",
-                    border: "1px darkgray solid",
-                  }}
-                >
-                  7
-                </ToggleButton>
-                <ToggleButton
-                  value={8}
-                  aria-label="8"
-                  name="score"
-                  sx={{
-                    width: "45px",
-                    height: "45px",
-                    border: "1px darkgray solid",
-                  }}
-                >
-                  8
-                </ToggleButton>
-                <ToggleButton
-                  value={9}
-                  aria-label="9"
-                  name="score"
-                  sx={{
-                    width: "45px",
-                    height: "45px",
-                    border: "1px darkgray solid",
-                  }}
-                >
-                  9
-                </ToggleButton>
-                <ToggleButton
-                  value={10}
-                  aria-label="10"
-                  name="score"
-                  sx={{
-                    width: "45px",
-                    height: "45px",
-                    borderRadius: 0,
-                    border: "1px darkgray solid",
-                  }}
-                >
-                  10
-                </ToggleButton>
-              </ToggleButtonGroup>
-            </Stack>
+								<ToggleButton
+									value={6}
+									aria-label="6"
+									name="score"
+									sx={{
+										width: "45px",
+										height: "45px",
+										border: "1px darkgray solid",
+									}}
+								>
+									6
+								</ToggleButton>
+								<ToggleButton
+									value={7}
+									aria-label="7"
+									name="score"
+									sx={{
+										width: "45px",
+										height: "45px",
+										border: "1px darkgray solid",
+									}}
+								>
+									7
+								</ToggleButton>
+								<ToggleButton
+									value={8}
+									aria-label="8"
+									name="score"
+									sx={{
+										width: "45px",
+										height: "45px",
+										border: "1px darkgray solid",
+									}}
+								>
+									8
+								</ToggleButton>
+								<ToggleButton
+									value={9}
+									aria-label="9"
+									name="score"
+									sx={{
+										width: "45px",
+										height: "45px",
+										border: "1px darkgray solid",
+									}}
+								>
+									9
+								</ToggleButton>
+								<ToggleButton
+									value={10}
+									aria-label="10"
+									name="score"
+									sx={{
+										width: "45px",
+										height: "45px",
+										borderRadius: 0,
+										border: "1px darkgray solid",
+									}}
+								>
+									10
+								</ToggleButton>
+							</ToggleButtonGroup>
+						</Stack>
 
 						<Box
 							sx={{
-								display: 'flex',
-								justifyContent: 'space-between',
-								mt: '0.5rem',
-								mb: '2rem',
+								display: "flex",
+								justifyContent: "space-between",
+								mt: "0.5rem",
+								mb: "2rem",
 							}}
 						>
-							<Typography variant="caption">0 = Extremely unlikely</Typography>
-							<Typography variant="caption">10 = Extremely likely</Typography>
+							{language === "FI" ? (
+								<div>
+									<Typography variant="caption">
+										0 = Erittäin epätodennäköistä
+									</Typography>
+									<Typography variant="caption">
+										10 = erittäin todennäköistä
+									</Typography>
+								</div>
+							) : language === "EN" ? (
+								<div>
+									<Typography variant="caption">
+										0 = Extremely unlikely
+									</Typography>
+									<Typography variant="caption">
+										10 = Extremely likely
+									</Typography>
+								</div>
+							) : language === "SV" ? (
+								<div>
+									<Typography variant="caption">
+										0 = Extremt osannolikt
+									</Typography>
+									<Typography variant="caption">
+										10 = Extremt troligt
+									</Typography>
+								</div>
+							) : (
+								""
+							)}
 						</Box>
 						{formik.errors.score ? (
 							<div className="error">{formik.errors.score}</div>
 						) : null}
 						<Box>
-							<label htmlFor="comment">
-								Please comment what is your reason for this score.
-							</label>
+							{language === "FI" ? (
+								<label htmlFor="comment">
+									Kommentoi syysi tähän pisteeseen
+								</label>
+							) : language === "EN" ? (
+								<label htmlFor="comment">
+									Please comment your reason for this score
+								</label>
+							) : language === "SV" ? (
+								<label htmlFor="comment">
+									Vänligen kommentera din anledning till denna poäng
+								</label>
+							) : (
+								""
+							)}
 
 							<TextField
 								name="comment"
@@ -301,30 +367,29 @@ const Form = () => {
 								disabled={isSubmitted}
 								inputProps={{ maxLength: commentCharacterLimit }}
 								helperText={`${formik.values.comment.length}/${commentCharacterLimit}`}
-								FormHelperTextProps={{ style: { textAlign: 'right' } }}
+								FormHelperTextProps={{ style: { textAlign: "right" } }}
 							/>
 						</Box>
 
+						<Box sx={{ display: "flex", justifyContent: "center", m: 2 }}>
+							<Button
+								onClick={formik.handleSubmit}
+								variant="contained"
+								color="warning"
+								disabled={isDisabled}
+							>
+								Send
+							</Button>
+						</Box>
+					</form>
 
-            <Box sx={{ display: "flex", justifyContent: "center", m: 2 }}>
-              <Button
-                onClick={formik.handleSubmit}
-                variant="contained"
-                color="warning"
-                disabled={isDisabled}
-              >
-                Send
-              </Button>
-            </Box>
-          </form>
-
-          <Typography variant="subtitle2" align="center">
-            Powered by The High Fives
-          </Typography>
-        </Paper>
-      </Container>
-    </Fragment>
-  );
+					<Typography variant="subtitle2" align="center">
+						Powered by The High Fives
+					</Typography>
+				</Paper>
+			</Container>
+		</Fragment>
+	);
 };
 
 export default Form;
